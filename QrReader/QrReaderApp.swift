e864d4bb08e82168c30ece766e8a1a6d726fbd25
Andrii Permiakov <andrusha20022002@gmail.com>
2022-11-03 18:23:35 +0200

    Initial Commit

---
+++ QrReader/QrReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrReaderApp.swift
+//  QrReader
+//
+//  Created by Пермяков Андрей on 03.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct QrReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
